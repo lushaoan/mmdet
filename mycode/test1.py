d642d3df7af65bc133b68c5491d8df4b05835d7e
lushaoan <422930673@qq.com>
2021-03-02 23:25:39 +0800

    add test code

--- mycode/test1.py
+++ mycode/test1.py
@@ -6,7 +6,20 @@ __date__ = '2020.10.25'
 __copyright__ = 'Copyright 2020, LSA'
 
 
-from mmdet.datasets.builder import DATASETS
-from mmdet.models.builder import LOSSES
+import sys
+sys.path.append('../')
+from mmdet.apis import init_detector, inference_detector, show_result_pyplot, show_result_ins
+import mmcv
 
-print(LOSSES)
+config_file = '../configs/yolo/yolov3_d53_mstrain-416_273e_coco.py'
+# download the checkpoint from model zoo and put it in `checkpoints/`
+checkpoint_file = '/media/lsa/MobileDisk2/open-mmlab/mmdetection_model/yolov3/yolov3_d53_mstrain-416_273e_coco-2b60fcd9.pth'
+
+# build the model from a config file and a checkpoint file
+model = init_detector(config_file, checkpoint_file, device='cuda:0')
+# print(model)
+# test a single image
+img = 'demo.jpg'
+result = inference_detector(model, img)
+
+show_result_ins(img, result, model.CLASSES, score_thr=0.25, out_file="demo_out.jpg")
